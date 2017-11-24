3b3c50bf7059808ae06e0ba0dfa9aa3957a6650f
Lan Layug <layug.al@linuxmail.org>
2017-11-24 10:31:59 +0800

    removed imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoPrefix = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-//const imageMin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
-// COPY HTML
 gulp.task('copyHTML', function(){
     gulp.src('src/*.html')
     .pipe(gulp.dest('build'))
 });
 
-// // minify images
-// gulp.task('imageMin', function(){
-//     gulp.src('src/images/*')
-//     .pipe(imageMin())
-//     .pipe(gulp.dest('build/images'))
-// });
-
-// sass and css
 gulp.task('sass', function() {
     gulp.src(['src/styles/*.scss'])
     .pipe(sass().on('error', sass.logError))
